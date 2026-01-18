01640703198c01254a83a936849f674d992f9824
Katerina <cuteinsect@yandex.ru>
2026-01-18 16:25:09 +0400

    fix

--- app/src/main/java/com/example/homeworkpm/MainActivity.kt
+++ app/src/main/java/com/example/homeworkpm/MainActivity.kt
@@ -1,20 +1,57 @@
-package com.example.homeworkpm
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View //
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.homeworkpm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main) // l
+
+        //
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        //
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Нажата кнопка Поиск",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+
+        //
+        val libraryClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Нажата кнопка Библиотека",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        //
+        val settingsClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Нажата кнопка Настройки",
+                    Toast.LENGTH_LONG // Пример использования LENGTH_LONG
+                ).show()
+            }
+        }
+
+        // Привязываем слушатели
+        searchButton.setOnClickListener(searchClickListener)
+        libraryButton.setOnClickListener(libraryClickListener)
+        settingsButton.setOnClickListener(settingsClickListener)
     }
 }
