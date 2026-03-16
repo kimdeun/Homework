e1c3e2863ba62ac1e586482fa258a6a63d33d88c
Katerina <cuteinsect@yandex.ru>
2026-03-16 05:11:16 -0500

    dz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,28 +28,37 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
+    implementation("com.google.android.material:material:1.11.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Glide для загрузки изображений
+    // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
 
     // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
--- app/src/main/java/com/example/homeworkpm/TrackActivity.kt
+++ app/src/main/java/com/example/homeworkpm/TrackActivity.kt
@@ -1,8 +1,24 @@
 package com.example.homeworkpm
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
     val trackName: String,
     val artistName: String,
     val trackTime: String,
     val artworkUrl100: String
-)
+) {
+    companion object {
+        fun fromDto(dto: TrackDto): Track {
+            return Track(
+                trackName = dto.trackName ?: "",
+                artistName = dto.artistName ?: "",
+                trackTime = dto.trackTimeMillis?.let {
+                    SimpleDateFormat("mm:ss", Locale.getDefault()).format(it)
+                } ?: "00:00",
+                artworkUrl100 = dto.artworkUrl100 ?: ""
+            )
+        }
+    }
+}
